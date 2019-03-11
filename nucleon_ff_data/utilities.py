aa4deda6cf30d3f0677fc7e4e28098ae683c28ab
Christopher Koerber <christopher@ckoerber.com>
2019-03-11 12:58:22 -0700

    Added file finder

--- nucleon_ff_data/utilities.py
+++ nucleon_ff_data/utilities.py
@@ -1,6 +1,11 @@
 """Utility routines for nucleon_ff_data module
 """
+from typing import List
+from typing import Optional
+
 import logging
+import re
+import os
 
 
 def set_up_logger(name: str) -> logging.Logger:
@@ -20,3 +25,40 @@ def set_up_logger(name: str) -> logging.Logger:
     ch.setFormatter(formatter)
     logger.addHandler(ch)
     return logger
+
+
+def has_match(string: str, patterns: List[str]) -> bool:
+    """Returns True if at least one of the regex patterns is matched by the file.
+
+    **Arguments**
+        string: str
+            The string to check.
+
+        patterns: List[str]
+            The regex patterns to match.
+    """
+    match = False
+    for pattern in patterns:
+        match = bool(re.findall(pattern, string))
+        if match:
+            continue
+    return match
+
+
+def find_all_files(root: str, patterns: Optional[List[str]] = None) -> List[str]:
+    """Recursivly iterates directory to all files which match the patterns.
+
+    **Arguments**
+        root: str
+            The string to check.
+
+        patterns: Optional[List[str]] = None
+            The regex patterns to match.
+    """
+    all_files = []
+    patterns = [] if patterns is None else patterns
+    for file_root, _, files in os.walk(root):
+        for file in files:
+            if has_match(file, patterns):
+                all_files.append(os.path.join(file_root, file))
+    return all_files
