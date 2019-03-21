7faba515f10dc037f326d04e58fa0a9453208221
Andre Walker-Loud <awalker-loud@lbl.gov>
2019-03-20 22:33:44 -0400

    METAQ_gflow works for a15m310L

--- scripts/METAQ_gflow.py
+++ scripts/METAQ_gflow.py
@@ -83,6 +83,8 @@ for c in cfgs_run:
         os.makedirs(base_dir+'/stdout/'+no)
     if not os.path.exists(base_dir+'/corrupt'):
         os.makedirs(base_dir+'/corrupt')
+    if not os.path.exists(base_dir+'/cfgs_flow'):
+        os.makedirs(base_dir+'/cfgs_flow')
 
     milc_cfg = base_dir+'/cfgs_scidac/'+params['ENS_LONG']+stream+'.'+no+'.scidac'
     cfg_flow = params['ENS_LONG']+stream+'.'+no+'_wflow'+params['FLOW_TIME']
@@ -91,12 +93,12 @@ for c in cfgs_run:
         if os.path.exists(milc_cfg):
             print('making flowed cfg input xml')
             metaq = 'cfg_flow_'+cfg_flow+'.sh'
-            metaq_file = metaq_dir +'/'+q+'/gpu/'+'/'+metaq
+            metaq_file = metaq_dir +'/'+q+'/cpu/'+'/'+metaq
             task_exist = False
             task_working = False
             if os.path.exists(metaq_file):
                 task_exist = True
-            for m_dir in ['todo/gpu','priority/gpu','hold']:
+            for m_dir in ['todo/cpu','priority/cpu','hold']:
                 if os.path.exists(metaq_dir+'/'+m_dir+'/'+metaq):
                     task_exist = True
             task_lst = glob(metaq_dir+'/working/*/*.sh')
@@ -115,7 +117,7 @@ for c in cfgs_run:
                 fin.write(xml_input.wflow_cfg % params)
                 ''' write cfg to disk '''
                 params['OBJ_ID']    = cfg_flow
-                params['OBJ_TYPE']  = 'LatticeColorMatrix'
+                params['OBJ_TYPE']  = 'Multi1dLatticeColorMatrixF'
                 params['LIME_FILE'] = cfg_file
                 fin.write(xml_input.qio_write % params)
                 ''' close xml file '''
