0144cc5901119edc4fc352e3f6006e98843e8a2e
Christopher Koerber <christopher@ckoerber.com>
2019-03-25 10:41:34 -0700

    Added file logger at DEBUG and logging of find all files.

--- nucleon_elastic_ff/utilities.py
+++ nucleon_elastic_ff/utilities.py
@@ -10,9 +10,10 @@ import os
 
 
 def set_up_logger(name: str) -> logging.Logger:
-    """Sets up command line logger
+    """Sets up command line and file logger.
 
-    Loggers default level is INFO. Only contains stdout logger.
+    Stream logger default level is INFO. File logger default level is DEBUG.
+    Only adds handlers if no handlers are present.
 
     **Arguments**
         name: str
@@ -21,12 +22,22 @@ def set_up_logger(name: str) -> logging.Logger:
     logger = logging.getLogger(name)
     if not logger.handlers:
         logger.setLevel(logging.DEBUG)
+        formatter = logging.Formatter("[%(asctime)s|%(name)s@%(levelname)s] %(message)s")
+
         ch = logging.StreamHandler()
         ch.setLevel(logging.INFO)
-        formatter = logging.Formatter("[%(asctime)s|%(name)s@%(levelname)s] %(message)s")
         ch.setFormatter(formatter)
         logger.addHandler(ch)
+
+        sh = logging.FileHandler(f"{name}.log", mode="a")
+        sh.setLevel(logging.DEBUG)
+        sh.setFormatter(formatter)
+        logger.addHandler(sh)
+
     return logger
+
+
+LOGGER = set_up_logger("nucleon_elastic_ff")
 
 
 def has_match(
@@ -79,6 +90,21 @@ def find_all_files(
     """
     all_files = []
 
+    LOGGER.info("Searching for files in `%s`", root)
+    if file_patterns:
+        LOGGER.info("\tFile patterns: `%s`", file_patterns)
+    if dir_patterns:
+        LOGGER.info("\tDir patterns: `%s`", dir_patterns)
+    if exclude_file_patterns:
+        LOGGER.info("\tAnd excluding files which match: `%s`", exclude_file_patterns)
+
+    LOGGER.info(
+        "All conditions must be fulfilled"
+        if match_all
+        else "Just one condition must match"
+    )
+
+    LOGGER.info("Matches:")
     for file_root, _, files in os.walk(root):
         if dir_patterns is None or has_match(
             file_root, dir_patterns, match_all=match_all
@@ -91,6 +117,9 @@ def find_all_files(
                     file, exclude_file_patterns
                 )
                 if file_match:
+                    LOGGER.debug("\t`%s`", os.path.join(file_root, file))
                     all_files.append(os.path.join(file_root, file))
+
+    LOGGER.info("Found %d files.", len(all_files))
 
     return all_files
