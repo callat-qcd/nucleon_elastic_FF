a99e8103340640b46f54cbbdad4456af08c3c2a4
walkloud <walksloud@gmail.com>
2019-03-27 19:56:47 -0700

    untested METAQ_coherent_formfac with machine aware scripts

--- scripts/METAQ_coherent_formfac.py
+++ scripts/METAQ_coherent_formfac.py
@@ -3,6 +3,9 @@ import os, sys, shutil, time
 from glob import glob
 import argparse
 
+'''
+    NUCLEON_ELASTIC_FF IMPORTS
+'''
 sys.path.append(os.path.join(os.path.dirname(__file__)))
 sys.path.append(os.path.join(os.path.dirname(__file__),'area51_files'))
 import xml_input
@@ -10,23 +13,29 @@ import metaq_input
 import importlib
 import c51_mdwf_hisq as c51
 import sources
+import utils
+import scheduler
 
-try:
-    ens_s = os.getcwd().split('/')[-2]
-except:
-    ens_s,junk = os.getcwd().split('/')[-2]
-ens,stream = ens_s.split('_')
+ens,stream = c51.ens_base()
+ens_s = ens+'_'+stream
 
 area51 = importlib.import_module(ens)
 params = area51.params
-ens_long=params['ENS_LONG']
-params['ENS_S'] = ens_s
+params['machine'] = c51.machine
+params['ENS_LONG'] = c51.ens_long[ens]
+params['ENS_S']    = ens_s
+params['STREAM']   = stream
+params['METAQ_PROJECT'] = 'cfg_flow_'+ens_s
 
+'''
+    COMMAND LINE ARG PARSER
+'''
 parser = argparse.ArgumentParser(description='make xml input for %s that need running' %sys.argv[0].split('/')[-1])
 parser.add_argument('cfgs',nargs='+',type=int,help='start [stop] run number')
 parser.add_argument('-s','--src',type=str)
 parser.add_argument('-o',default=False,action='store_const',const=True,\
     help='overwrite xml and metaq files? [%(default)s]')
+parser.add_argument('--mtype',default='cpu',help='specify metaq dir [%(default)s]')
 parser.add_argument('-t','--t_sep',nargs='+',type=int,help='values of t_sep [default = all]')
 parser.add_argument('-d','--debug',default=False,action='store_const',const=True,\
     help='run DEBUG? [%(default)s]')
@@ -38,75 +47,35 @@ print('%s: Arguments passed' %sys.argv[0].split('/')[-1])
 print(args)
 print('')
 
-''' time in minutes to define "old" file '''
-time_delete = params['prop_time_delete']
-
-ri = args.cfgs[0]
-if len(args.cfgs) == 1:
-    rf = ri+1
-    dr = 1
-elif len(args.cfgs) == 2:
-    rf = args.cfgs[1]+1
-    dr = 1
-else:
-    rf = args.cfgs[1]+1
-    dr = args.cfgs[2]
-cfgs_run = range(ri,rf,dr)
-
-''' BUILD SRC DICTIONARY '''
-nt = int(params['NT'])
-nl = int(params['NL'])
-if args.src:
-    if len(args.cfgs) > 1:
-        print('if a src is passed, only 1 cfg can be specified which is presumably the right one')
-        sys.exit(-1)
-    else:
-        cfgs = args.cfgs
-        srcs = {int(args.cfgs[0]):[args.src]}
-else:
-    srcs = {}
-    for c in cfgs_run:
-        no = str(c)
-        srcs_cfg = sources.make(no, nl=nl, nt=nt, t_shifts=params['t_shifts'],
-            generator=params['generator'], seed=params['seed'][stream])
-        srcs[c] = []
-        for origin in srcs_cfg:
-            try:
-                src_gen = srcs_cfg[origin].iteritems()
-            except AttributeError: # Python 3 automatically creates a generator
-                src_gen = srcs_cfg[origin].items()
-            for src_type, src in src_gen:
-                srcs[c].append(sources.xXyYzZtT(src))
-print('running ',cfgs_run[0],'-->',cfgs_run[-1])
+'''
+    RUN PARAMETER SET UP
+'''
+cfgs_run,srcs = utils.parse_cfg_src_argument(args.cfgs,args.src,params)
 
 if args.priority:
-    priority = '-p'
     q = 'priority'
+    params['PRIORITY'] = '-p'
 else:
-    priority = ''
     q = 'todo'
-params['PRIORITY'] = priority
+    params['PRIORITY'] = ''
+
+nt = int(params['NT'])
+nl = int(params['NL'])
+
+print('running ',cfgs_run[0],'-->',cfgs_run[-1])
 
 smr = 'gf'+params['FLOW_TIME']+'_w'+params['WF_S']+'_n'+params['WF_N']
 val = smr+'_M5'+params['M5']+'_L5'+params['L5']+'_a'+params['alpha5']
+''' for now - just doing the light quark '''
 params['MQ'] = params['MV_L']
-
-base_dir = c51.base_dir % params
-params['ENS_DIR']    = c51.ens_dir % params
-params['SCRIPT_DIR'] = c51.script_dir
-cfg_dir = base_dir+'/cfgs_flow'
-metaq_dir  = c51.metaq_dir
-params['METAQ_DIR'] = metaq_dir
 
 if args.t_sep == None:
     t_seps  = params['t_seps']
 else:
     t_seps = args.t_sep
-flavs = params['flavs']
-spins = params['spins']
 flav_spin = []
-for f in flavs:
-    for s in spins:
+for f in params['flavs']:
+    for s in params['spins']:
         flav_spin.append(f+'_'+s)
 ''' ONLY doing snk_mom 0 0 0 now '''
 snk_mom = params['snk_mom'][0]
@@ -116,14 +85,24 @@ params['M1']=m1
 params['M2']=m2
 params['MOM'] = 'px%spy%spz%s' %(m0,m1,m2)
 
-particles = params['particles']
+params = area51.mpirun_params(c51.machine)
+params['NODES']       = params['cpu_nodes']
+params['METAQ_NODES'] = params['cpu_nodes']
+params['METAQ_GPUS']  = params['cpu_gpus']
+params['WALL_TIME']   = params['gflow_time']
+params['ENS_DIR']     = c51.ens_dir % params
+params['SCRIPT_DIR']  = c51.script_dir
+params['MAXCUS']      = params['cpu_maxcus']
+params['SOURCE_ENV']  = c51.env
+params['PROG']        = '$LALIBE_CPU'
+params['APP']         = 'APP='+c51.bind_dir+params['cpu_bind']
+params['NRS']         = params['cpu_nrs']
+params['RS_NODE']     = params['cpu_rs_node']
+params['A_RS']        = params['cpu_a_rs']
+params['G_RS']        = params['cpu_g_rs']
+params['C_RS']        = params['cpu_c_rs']
+params['L_GPU_CPU']   = params['cpu_latency']
 
-coherent_ff_base = c51.coherent_ff_base
-seqprop_base     = c51.seqprop_base
-seqprop_size     = int(nt)* int(nl)**3 * 3**2 * 4**2 * 2 * 4
-sp_ext           = params['SP_EXTENSION']
-prop_base        = c51.prop_base
-print(params)
 n_curr = len(params['curr_4d'])
 n_flav = len(params['flavs'])
 n_spin = len(params['spins'])
@@ -133,38 +112,19 @@ coherent_ff_size_4d = n_curr * n_flav * n_spin * n_par *int(nt)*int(nl)**3 * 2*8
 for c in cfgs_run:
     no = str(c)
     params['CFG'] = no
-    if len(srcs[c]) == params['N_SEQ']:
-        all_srcs = True
-    else:
-        all_srcs = False
-    cfg_file = cfg_dir+'/'+ens_long+stream+'.'+no+'_wflow1.0.lime'
-    if os.path.exists(cfg_file) and all_srcs:
-        params.update({'CFG_FILE':cfg_file})
+    ''' set up ensemble and make sure all dirs exist '''
+    params = c51.ensemble(params)
+    params['RUN_DIR']     = params['prod']
+
+    ''' check if flowed cfg exists and make if not '''
+    scidac_cfg = params['scidac_cfg']
+    cfg_flow   = params['ENS_LONG']+stream+'.'+no+'_wflow'+params['FLOW_TIME']
+    cfg_file   = params['flowed_cfg']
+
+    if os.path.exists(cfg_file):
+        params['CFG_FILE'] = cfg_file
         print("Checking coherent formfactor for cfg: ",c)
 
-        if not os.path.exists(base_dir+'/xml/'+no):
-            os.makedirs(base_dir+'/xml/'+no)
-        if not os.path.exists(base_dir+'/stdout/'+no):
-            os.makedirs(base_dir+'/stdout/'+no)
-        if not os.path.exists(base_dir+'/formfac/'+no):
-            os.makedirs(base_dir+'/formfac/'+no)
-        if not os.path.exists(base_dir+'/formfac_4D/'+no):
-            os.makedirs(base_dir+'/formfac_4D/'+no)
-        if not os.path.exists(base_dir+'/corrupt'):
-            os.makedirs(base_dir+'/corrupt')
-
-        # make sure all props are ready
-        '''
-        all_props=True
-        for s0 in srcs[c]:
-            prop_name = prop_base %{'CFG':no,'SRC':s0}
-            prop_file = base_dir+'/props/'+no + '/' + prop_name+'.'+sp_ext
-            if not os.path.exists(prop_file):
-                print('    missing',prop_file)
-                all_props=False
-        To improve load balancing of work, we are splitting each src to a different task
-        '''
-        #if all_props:
         have_all_seqprops=True
         for dt_int in t_seps:
             dt = str(dt_int)
@@ -183,14 +143,10 @@ for c in cfgs_run:
                         params['T_SEP'] = '-'+dt
                     else:
                         params['T_SEP'] = dt
-                    seqprop_name  = seqprop_base % params
-                    seqprop_file  = base_dir+'/seqprop/'+no+'/'+seqprop_name+'.'+sp_ext
-                    if os.path.exists(seqprop_file) and os.path.getsize(seqprop_file) < seqprop_size:
-                        now = time.time()
-                        file_time = os.stat(seqprop_file).st_mtime
-                        if (now-file_time)/60 > time_delete:
-                            print('DELETING BAD PROP',os.path.getsize(seqprop_file),seqprop_file.split('/')[-1])
-                            shutil.move(seqprop_file,seqprop_file.replace('seqprop/'+no+'/','corrupt/'))
+                    seqprop_name = c51.names['seqprop'] %params
+                    seqprop_file = params['seqprop']+'/'+seqprop_name+'.'+params['SP_EXTENSION']
+                    seqprop_size = int(nt)* int(nl)**3 * 3**2 * 4**2 * 2 * 4
+                    utils.check_file(seqprop_file,seqprop_size,params['file_time_delete'],params['corrupt'])
                     if not os.path.exists(seqprop_file):
                         print('    missing:',seqprop_file)
                         all_seqprops=False
@@ -202,39 +158,19 @@ for c in cfgs_run:
                     ''' Does the 3pt file exist? '''
                     ''' dt and -dt in same file, labled by dt '''
                     params['T_SEP'] = dt
-                    coherent_formfac_name  = coherent_ff_base % params
-                    coherent_formfac_file  = base_dir+'/formfac/'+no + '/'+coherent_formfac_name+'.h5'
-                    coherent_formfac_file_4D = coherent_formfac_file.replace('formfac_','formfac_4D_').replace('/formfac/','/formfac_4D/')
+                    coherent_formfac_name  = c51.names['coherent_ff'] % params
+                    coherent_formfac_file  = params['formfac'] +'/'+coherent_formfac_name + '.h5'
+                    coherent_formfac_file_4D = coherent_formfac_file.replace('formfac_','formfac_4D_')
                     params['THREE_PT_FILE'] = coherent_formfac_file
                     params['THREE_PT_FILE_4D'] = coherent_formfac_file_4D
-                    if os.path.exists(coherent_formfac_file_4D) and os.path.getsize(coherent_formfac_file_4D) < coherent_ff_size_4d:
-                        now = time.time()
-                        file_time = os.stat(coherent_formfac_file_4D).st_mtime
-                        if (now-file_time)/60 > time_delete:
-                            print('DELETING BAD COHERENT_FF',os.path.getsize(coherent_formfac_file_4D),coherent_formfac_file_4D.split('/')[-1])
-                            shutil.move(coherent_formfac_file_4D,coherent_formfac_file_4D.replace('formfac_4D/'+no+'/','corrupt/'))
-                            shutil.move(coherent_formfac_file,coherent_formfac_file.replace('formfac/'+no+'/','corrupt/'))
-                            #sys.exit()
+                    utils.check_file(coherent_formfac_file_4D,coherent_ff_size_4d,params['file_time_delete'],params['corrupt'])
                     if not os.path.exists(coherent_formfac_file) and not os.path.exists(coherent_formfac_file_4D):
                         # loop over FLAV and SPIN as all in 1 file
                         metaq  = coherent_formfac_name+'.sh'
-                        metaq_file = metaq_dir +'/'+q+'/cpu/'+metaq
-                        task_exist = False
-                        task_working = False
-                        if os.path.exists(metaq_file):
-                            task_exist = True
-                        for m_dir in ['todo/cpu','priority/cpu','hold']:
-                            if os.path.exists(metaq_dir+'/'+m_dir+'/'+metaq):
-                                task_exist = True
-                        task_lst = glob(metaq_dir+'/working/*/*.sh')
-                        task_lst += glob(metaq_dir+'/working/*/*/*.sh')
-                        for task in task_lst:
-                            if metaq == task.split('/')[-1]:
-                                task_exist = True
-                                task_working = True
-                        if not task_exist or (args.o and task_exist and not task_working):
-                            prop_name = prop_base % params
-                            prop_file = base_dir+'/prop/'+no+'/'+prop_name+'.'+sp_ext
+                        t_e,t_w = scheduler.check_task(metaq,args.mtype,params,folder=q,overwrite=args.o)
+                        if not t_e or (args.o and not t_w):
+                            prop_name = c51.names['prop'] % params
+                            prop_file = params['prop'] + '/' + prop_name+'.'+params['SP_EXTENSION']
                             if os.path.exists(prop_file):
                                 xmlini = coherent_formfac_file.replace('/formfac/','/xml/').replace('.h5','.ini.xml')
                                 fin = open(xmlini,'w')
@@ -264,14 +200,14 @@ for c in cfgs_run:
                                         params['SINK_SPIN']=src_spin
                                         spin = snk_spin+'_'+src_spin
                                         params['FLAV_SPIN']=fs
-                                        seqprop_name  = seqprop_base % params
-                                        seqprop_file  = base_dir+'/seqprop/'+no+'/'+seqprop_name+'.'+sp_ext
+                                        seqprop_name  = c51.names['seqprop'] %params
+                                        seqprop_file  = params['seqprop']+'/'+seqprop_name+'.'+params['SP_EXTENSION']
                                         params['LIME_FILE'] = seqprop_file
                                         params['OBJ_ID']    = seqprop_name
-                                        params['SEQPROP_'+fs] = seqprop_base % params
+                                        params['SEQPROP_'+fs] = seqprop_name
                                         fin.write(xml_input.qio_read % params)
                                     #for s0 in srcs[c]:
-                                    prop_name = prop_base % params
+                                    prop_name = c51.names['prop'] % params
                                     params['PROP_NAME'] = prop_name
 
                                     ''' make 3pt contractions '''
@@ -298,18 +234,13 @@ for c in cfgs_run:
                                 fin.write(xml_input.tail % params)
                                 fin.close()
 
-                                # Make METAQ task
-                                params.update({
-                                    'XML_IN':xmlini,'XML_OUT':xmlini.replace('.ini.xml','.out.xml'),
-                                    'STDOUT':xmlini.replace('.ini.xml','.stdout').replace('/xml/','/stdout/'),
-                                    'METAQ_LOG':metaq_dir+'/log/'+metaq.replace('.sh','.log'),
-                                    'BASE_DIR':base_dir,'METAQ_RUN':metaq_file,'METAQ_FINISHED':metaq_file.replace('runs','finished')
-                                    })
-                                m_in = open(metaq_file,'w')
-                                m_in.write(metaq_input.formfac_contractions % params)
-                                m_in.close()
-                                os.chmod(metaq_file,0o770)
-                                print('    making task:',metaq)
+                                ''' Make METAQ task '''
+                                params['METAQ_LOG'] = params['METAQ_DIR']+'/log/'+metaq.replace('.sh','.log')
+                                params['INI']       = xmlini
+                                params['OUT']       = xmlini.replace('.ini.xml','.out.xml')
+                                params['STDOUT']    = xmlini.replace('.ini.xml','.stdout').replace('/xml/','/stdout/')
+                                params['CLEANUP']   = ''
+                                scheduler.make_task(metaq,args.mtype,params,folder=q)
                             else:
                                 print('MISSING prop',prop_file)
                         else:
@@ -322,7 +253,7 @@ for c in cfgs_run:
                 print('    missing FLAV or SPIN seqprops, dt=',dt)
         if not have_all_seqprops:
             print('    missing FLAV or SPIN seqprops')
-            os.system('python %s/METAQ_coherent_seqprop.py %s %s -v' %(params['SCRIPT_DIR'],c,priority))
+            os.system('python %s/METAQ_coherent_seqprop.py %s %s -v' %(params['SCRIPT_DIR'],c,params['PRIORITY']))
         #else:
         #    print('    missing props')
     else:
