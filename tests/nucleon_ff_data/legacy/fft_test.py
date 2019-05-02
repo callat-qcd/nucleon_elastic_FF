3a8e0018d29c2bc4b3db0bf3435140a6de37ebbc
Christopher Koerber <christopher@ckoerber.com>
2019-05-02 14:14:47 -0700

    Added fft legacy unittest

---
+++ tests/nucleon_ff_data/legacy/fft_test.py
@@ -0,0 +1,81 @@
+"""Compares tsliced routines against results used for export.
+"""
+from os.path import join
+
+from unittest import TestCase
+
+from nucleon_elastic_ff.test_utilities import CommandTest
+from nucleon_elastic_ff.test_utilities import TMPDIR
+from nucleon_elastic_ff.test_utilities import LOGGER
+
+from nucleon_elastic_ff.data.scripts.fft import fft_file
+
+
+class FormfacTsliceTest(CommandTest, TestCase):
+    """Runs fft on legacy ``formfac_4D`` files and compares results"""
+
+    link_files = [
+        join(
+            "formfac_4D_tslice_src_avg",
+            "formfac_4D_tslice_src_avg_px0py0pz0_Nsnk1_src_avg.h5",
+        )
+    ]
+    check_files = [
+        join(
+            "formfac_4D_tslice_src_avg",
+            "formfac_4D_tslice_src_avg_px0py0pz0_Nsnk1_src_avg_fft.h5",
+        )
+    ]
+
+    @staticmethod
+    def command():
+        """Command which will be executed by the unittest.
+        """
+        LOGGER.info("Running `fft`")
+        fft_file(
+            join(
+                TMPDIR,
+                "formfac_4D_tslice_src_avg",
+                "formfac_4D_tslice_src_avg_px0py0pz0_Nsnk1_src_avg.h5",
+            ),
+            join(
+                TMPDIR,
+                "formfac_4D_tslice_src_avg",
+                "formfac_4D_tslice_src_avg_px0py0pz0_Nsnk1_src_avg_fft.h5",
+            ),
+            overwrite=False,
+            max_momentum=2,
+            cuda=False,
+        )
+
+
+class SpecTsliceTest(CommandTest, TestCase):
+    """Runs fft on legacy ``spec_4D`` files and compares results"""
+
+    link_files = [
+        join("spec_4D_tslice_avg", "spec_4D_tslice_avg_px0py0pz0_Nsnk1_src_avg.h5")
+    ]
+    check_files = [
+        join("spec_4D_tslice_avg", "spec_4D_tslice_avg_px0py0pz0_Nsnk1_src_avg_fft.h5")
+    ]
+
+    @staticmethod
+    def command():
+        """Command which will be executed by the unittest.
+        """
+        LOGGER.info("Running `fft`")
+        fft_file(
+            join(
+                TMPDIR,
+                "spec_4D_tslice_avg",
+                "spec_4D_tslice_avg_px0py0pz0_Nsnk1_src_avg.h5",
+            ),
+            join(
+                TMPDIR,
+                "spec_4D_tslice_avg",
+                "spec_4D_tslice_avg_px0py0pz0_Nsnk1_src_avg_fft.h5",
+            ),
+            overwrite=False,
+            max_momentum=2,
+            cuda=False,
+        )
