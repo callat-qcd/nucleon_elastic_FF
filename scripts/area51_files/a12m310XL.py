837a8c5462da4b14252d7fdda92c3c92602958d2
Andre Walker-Loud <awalker-loud@lbl.gov>
2019-05-03 18:48:46 -0400

    updating a12m310XL file

--- scripts/area51_files/a12m310XL.py
+++ scripts/area51_files/a12m310XL.py
@@ -125,7 +125,8 @@ def mpirun_params(machine):
         params['gpu_metaq_nodes'] = 0
         params['gpu_gpus']    = 6
         params['gpu_maxcus']  = 1
-        params['prop_time']   = 10
+        params['prop_time']   = 24
+        params['seqprop_time']   = 15
 
         params['gpu_nrs']     = '--nrs 1'
         params['gpu_rs_node'] = '-r1'
