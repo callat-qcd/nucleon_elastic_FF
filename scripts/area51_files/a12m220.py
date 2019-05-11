2ce610222d38d019f14204a00da101aa9840ec1d
Andre Walker-Loud <awalker-loud@lbl.gov>
2019-05-10 20:10:11 -0400

    updating a09m220 for summit; starting a12m220

--- scripts/area51_files/a12m220.py
+++ scripts/area51_files/a12m220.py
@@ -1,34 +1,39 @@
 import sources
 
 params = dict()
-params['ENS_ABBR'] = 'a09m220'
-params['ENS_LONG'] = 'l4896f211b630m00363m0363m430'
-params['NL']   = '48'
-params['NT']   = '96'
-params['U0']   = '0.874164'
-params['MS_L'] = '0.00363'
-params['MS_S'] = '0.0363'
-params['MS_C'] = '0.430'
+params['tuning_mq'] = False
+params['run_ff'] = True
+
+params['ENS_ABBR'] = 'a12m220'
+params['NL']   = '32'
+params['NT']   = '64'
+params['U0']   = '0.86372'
+params['MS_L'] = '0.00507'
+params['MS_S'] = '0.0507'
+params['MS_C'] = '0.628'
+params['cfg_i'] = 300
+params['cfg_f'] = 5295
+params['cfg_d'] = 5
 
 params['FLOW_TIME'] = '1.0'
 params['FLOW_STEP'] = '40'
-params['WF_S'] = '3.5'
-params['WF_N'] = '45'
+params['WF_S'] = '3.0'
+params['WF_N'] = '30'
 
-params['M5'] = '1.1'
-params['L5'] = '8'
+params['M5'] = '1.2'
+params['L5'] = '12'
 ''' NOTE: b5-c5 = 1 for all our tuning '''
-params['B5']     = '1.25'
-params['C5']     = '0.25'
+params['B5']     = '1.5'
+params['C5']     = '0.5'
 params['alpha5'] = '1.5'
 
-params['MV_L'] = '0.00449'
-params['MV_S'] = '0.0491'
+params['MV_L'] = '0.00600'
+params['MV_S'] = 
 
-params['spec_size'] = 51312
-params['ff_size']   = 0
+params['spec_size'] = 
+params['ff_size']   = 
 
-params['MAX_ITER']   = '5000'
+params['MAX_ITER']   = '4000'
 params['RSD_TARGET'] = '1.e-7'
 params['Q_DELTA']    = '0.1'
 params['RSD_TOL']    = '80'
@@ -37,18 +42,18 @@ params['SP_EXTENSION'] = 'lime'
 
 params['seed'] = dict()
 params['seed']['a'] = '1a'
-'''                               0, nt/2, nt/4, 3 nt/4 '''
-params['t_shifts'] = [ 0, 48  , 24  , 72     ]
+'''                    0, nt/2, nt/4, 3 nt/4 '''
+params['t_shifts'] = [ 0, 32  , 16  , 48     ]
 params['generator'] = sources.oa(int(params['NL']))
 
 ''' minutes after last file modification time when deletion of small files is OK '''
-params['file_time_delete'] = 10
-
+params['file_time_delete'] = 10.
 
 params['MESONS_PSQ_MAX']  = 5
 params['BARYONS_PSQ_MAX'] = 0
 
-params['t_seps']  = [3,4,5,6,7,8,9,10,11]
+params['run_3pt'] = True
+params['t_seps']  = [3,4,5,6,7,8,9]
 params['flavs']   = ['UU','DD']
 params['spins']   = ['up_up','dn_dn']
 params['snk_mom'] = ['0 0 0']
@@ -75,16 +80,18 @@ def mpirun_params(machine):
 
         params['cpu_nrs']     = '--nrs 2'
         params['cpu_rs_node'] = '-r2'
-        params['cpu_a_rs']    = '-a18'
+        params['cpu_a_rs']    = '-a16'
         params['cpu_g_rs']    = ''
-        params['cpu_c_rs']    = '-c18'
+        params['cpu_c_rs']    = '-c16'
         params['cpu_latency'] = '-l cpu-cpu'
-        params['cpu_bind']    = 'lassen_bind_cpu.N36.sh'
+        params['cpu_bind']    = 'lassen_bind_cpu.N32.sh'
 
         params['gpu_nodes']   = 0
+        params['gpu_metaq_nodes'] = 0
         params['gpu_gpus']    = 4
         params['gpu_maxcus']  = 1
         params['prop_time']   = 10
+        params['seqprop_time']   = 10
 
         params['gpu_nrs']     = '--nrs 1'
         params['gpu_rs_node'] = '-r1'
@@ -93,38 +100,34 @@ def mpirun_params(machine):
         params['gpu_c_rs']    = '-c4'
         params['gpu_latency'] = '-l gpu-cpu'
         params['gpu_geom']    = ' -geom 1 1 1 4'
+        params['gpu_bind']    = 'lassen_bind_gpu.omp4.sh'
 
     if machine == 'summit':
-        params['metaq_split'] = True
-        params['cpu_nodes']   = 2
+        params['cpu_nodes']   = 1
         params['cpu_gpus']    = 0
         params['cpu_maxcus']  = 1
         params['gflow_time']  = 15
         params['src_time']    = 5
         params['spec_time']   = 10
 
-        params['cpu_nrs']     = '--nrs 4'
+        params['cpu_nrs']     = '--nrs 2'
         params['cpu_rs_node'] = '-r2'
         params['cpu_a_rs']    = '-a16'
         params['cpu_g_rs']    = ''
         params['cpu_c_rs']    = '-c16'
         params['cpu_latency'] = '-l cpu-cpu'
-        params['cpu_bind']    = ''
 
-        params['gpu_nodes']   = 2
-        params['gpu_metaq_nodes'] = 0
-        params['gpu_gpus']    = 12
+        params['gpu_nodes']   = 0
+        params['gpu_gpus']    = 6
         params['gpu_maxcus']  = 1
-        params['prop_time']   = 25
-        params['seqprop_time'] = 18
+        params['prop_time']   = 10
 
-        params['gpu_nrs']     = '--nrs 2'
+        params['gpu_nrs']     = '--nrs 1'
         params['gpu_rs_node'] = '-r1'
         params['gpu_a_rs']    = '-a6'
         params['gpu_g_rs']    = '-g6'
         params['gpu_c_rs']    = '-c6'
         params['gpu_latency'] = '-l gpu-cpu'
-        params['gpu_geom']    = ' -geom 1 1 1 12'
-        params['gpu_bind']    = ''
+        params['gpu_geom']    = ' -geom 1 1 1 6'
 
     return params
