27cab960cb7d746c95e1596f9d9bfa390da995d1
Christopher Koerber <christopher@ckoerber.com>
2019-06-07 09:51:12 -0700

    Added chunkwise iteratrion option to fft module + unittest

--- tests/nucleon_ff_data/legacy/fft_test.py
+++ tests/nucleon_ff_data/legacy/fft_test.py
@@ -11,7 +11,7 @@ from nucleon_elastic_ff.test_utilities import LOGGER
 from nucleon_elastic_ff.data.scripts.fft import fft_file
 
 
-class FormfacTsliceTest(CommandTest, TestCase):
+class FormfacFFTTest(CommandTest, TestCase):
     """Runs fft on legacy ``formfac_4D`` files and compares results"""
 
     link_files = [
@@ -49,7 +49,46 @@ class FormfacTsliceTest(CommandTest, TestCase):
         )
 
 
-class SpecTsliceTest(CommandTest, TestCase):
+class FormfacChunkedFFTTest(CommandTest, TestCase):
+    """Runs chunked fft on legacy ``formfac_4D`` files and compares results"""
+
+    link_files = [
+        join(
+            "formfac_4D_tslice_src_avg",
+            "formfac_4D_tslice_src_avg_px0py0pz0_Nsnk1_src_avg.h5",
+        )
+    ]
+    check_files = [
+        join(
+            "formfac_4D_tslice_src_avg",
+            "formfac_4D_tslice_src_avg_px0py0pz0_Nsnk1_src_avg_fft.h5",
+        )
+    ]
+
+    @staticmethod
+    def command():
+        """Command which will be executed by the unittest.
+        """
+        LOGGER.info("Running `fft`")
+        fft_file(
+            join(
+                TMPDIR,
+                "formfac_4D_tslice_src_avg",
+                "formfac_4D_tslice_src_avg_px0py0pz0_Nsnk1_src_avg.h5",
+            ),
+            join(
+                TMPDIR,
+                "formfac_4D_tslice_src_avg",
+                "formfac_4D_tslice_src_avg_px0py0pz0_Nsnk1_src_avg_fft.h5",
+            ),
+            overwrite=False,
+            max_momentum=2,
+            chunk_size=2,
+            cuda=False,
+        )
+
+
+class SpecFFTTest(CommandTest, TestCase):
     """Runs fft on legacy ``spec_4D`` files and compares results"""
 
     link_files = [
