ed7393cedfc76052708c17d708f6c53e0d0210fb
Christopher Koerber <christopher@ckoerber.com>
2019-07-12 17:06:24 -0700

    First example draft for integrating lattedb

---
+++ quarkson/area51_files/a12m130.py
@@ -0,0 +1,137 @@
+import sources
+
+params = dict()
+params['tuning_mq'] = False
+params['run_ff'] = True
+
+# the params['si','sf','ds'] are now handled in the sources.py file - srcs here overide those defaults
+# you must specify all three of these params to override the default
+
+params['ENS_ABBR'] = 'a12m130'
+params['NL']   = '48'
+params['NT']   = '64'
+params['U0']   = '0.86372'
+params['MS_L'] = '0.00184'
+params['MS_S'] = '0.0507'
+params['MS_C'] = '0.628'
+params['cfg_i'] = 300
+params['cfg_f'] = 5295
+params['cfg_d'] = 5
+
+params['FLOW_TIME'] = '1.0'
+params['FLOW_STEP'] = '40'
+params['WF_S'] = '3.0'
+params['WF_N'] = '30'
+
+params['M5'] = '1.2'
+params['L5'] = '20'
+''' NOTE: b5-c5 = 1 for all our tuning '''
+params['B5']     = '2.0'
+params['C5']     = '1.0'
+params['alpha5'] = '3.0'
+
+params['MV_L'] = '0.00195'
+params['MV_S'] = '0.0693'
+
+params['spec_size'] = 1
+params['ff_size']   = 1
+
+params['MAX_ITER']   = '15000'
+params['RSD_TARGET'] = '1.e-7'
+params['Q_DELTA']    = '0.1'
+params['RSD_TOL']    = '80'
+
+params['SP_EXTENSION'] = 'lime'
+
+params['seed'] = dict()
+params['seed']['a'] = '1a'
+'''                    0, nt/2, nt/4, 3 nt/4 '''
+params['t_shifts'] = [ 0, 32  , 16  , 48     ]
+params['generator'] = sources.oa(int(params['NL']))
+
+''' minutes after last file modification time when deletion of small files is OK '''
+params['file_time_delete'] = 10.
+
+params['MESONS_PSQ_MAX']  = 0
+params['BARYONS_PSQ_MAX'] = 0
+
+params['run_3pt'] = True
+params['t_seps']  = [3,4,5,6,7,8,9,10,11]
+params['flavs']   = ['UU','DD']
+params['spins']   = ['up_up','dn_dn']
+params['snk_mom'] = ['0 0 0']
+params['SS_PS']   = 'SS'
+params['particles'] = ['proton','proton_np']
+params['curr_4d'] = ['A3','V4','A1','A2','A4','V1','V2','V3','P','S']
+#params['curr_p']  = ['A3','V4','A1','A2','A4','V1','V2','V3','P','S']
+params['curr_0p'] = ['A3','V4','A1','A2','A4','V1','V2','V3','S','T34','T12','CHROMO_MAG']
+
+''' SCHEDULING PARAMETERS '''
+
+params['queue'] = 'metaq'
+
+params['OMP_NUM_THREADS'] = '4'
+
+def mpirun_params(machine):
+    if machine == 'lassen':
+        params['metaq_split'] = True
+        params['cpu_nodes']   = 1
+        params['cpu_gpus']    = 0
+        params['cpu_maxcus']  = 1
+        params['gflow_time']  = 15
+        params['src_time']    = 5
+        params['spec_time']   = 13
+
+        params['cpu_nrs']     = '--nrs 2'
+        params['cpu_rs_node'] = '-r2'
+        params['cpu_a_rs']    = '-a16'
+        params['cpu_g_rs']    = ''
+        params['cpu_c_rs']    = '-c16'
+        params['cpu_latency'] = '-l cpu-cpu'
+        params['cpu_bind']    = 'lassen_bind_cpu.N32.sh'
+
+        params['gpu_nodes']   = 1
+        params['gpu_metaq_nodes'] = 0
+        params['gpu_gpus']    = 4
+        params['gpu_maxcus']  = 1
+        params['prop_time']   = 10
+        params['seqprop_time']   = 10
+
+        params['gpu_nrs']     = '--nrs 1'
+        params['gpu_rs_node'] = '-r1'
+        params['gpu_a_rs']    = '-a4'
+        params['gpu_g_rs']    = '-g4'
+        params['gpu_c_rs']    = '-c4'
+        params['gpu_latency'] = '-l gpu-cpu'
+        params['gpu_geom']    = ' -geom 1 1 1 4'
+        params['gpu_bind']    = 'lassen_bind_gpu.omp4.sh'
+
+    if machine == 'summit':
+        params['cpu_nodes']   = 2
+        params['cpu_gpus']    = 0
+        params['cpu_maxcus']  = 1
+        params['gflow_time']  = 15
+        params['src_time']    = 5
+        params['spec_time']   = 10
+
+        params['cpu_nrs']     = '--nrs 4'
+        params['cpu_rs_node'] = '-r2'
+        params['cpu_a_rs']    = '-a16'
+        params['cpu_g_rs']    = ''
+        params['cpu_c_rs']    = '-c16'
+        params['cpu_latency'] = '-l cpu-cpu'
+
+        params['gpu_nodes']   = 0
+        params['gpu_gpus']    = 12
+        params['gpu_maxcus']  = 1
+        params['prop_time']   = 90
+
+        params['gpu_nrs']     = '--nrs 2'
+        params['gpu_rs_node'] = '-r1'
+        params['gpu_a_rs']    = '-a6'
+        params['gpu_g_rs']    = '-g6'
+        params['gpu_c_rs']    = '-c6'
+        params['gpu_latency'] = '-l gpu-cpu'
+        params['gpu_geom']    = ' -geom 1 1 3 4'
+
+    return params
---
+++ quarkson/utils.py
@@ -0,0 +1,181 @@
+import os, sys, time, shutil
+import numpy as np
+
+import sources
+
+# A little template that looks all the way through symbolic links to see if there's anything down at the bottom.
+def recursiveLinkCheck(existence_checker):
+    def existsQ(path):
+        real_path = os.path.realpath(path)
+        if existence_checker(real_path):
+            return real_path
+        return False
+    return existsQ
+
+# Use the template to build functions that check for files and directories at the bottom:
+dirExists =  recursiveLinkCheck(os.path.isdir)
+def fileExists(path):
+    return recursiveLinkCheck(os.path.isfile)(path) and os.path.getsize(path) > 0
+
+def ensure_dirExists(path):
+    if not dirExists(path):
+        if not fileExists(path):
+            os.makedirs(path)
+        else:
+            print("You tried to create a directory that is already the name of a file.")
+            print("Exiting for safety.")
+            exit(-1)
+
+def check_file(f_name,f_size,time_delete,bad_file_dir):
+    if os.path.exists(f_name) and os.path.getsize(f_name) < f_size:
+        now = time.time()
+        file_time = os.stat(f_name).st_mtime
+        ''' check last update of file in minutes '''
+        if (now-file_time)/60 > time_delete:
+            print('DELETING BAD FILE',os.path.getsize(f_name),f_name.split('/')[-1])
+            shutil.move(f_name,bad_file_dir+'/'+f_name.split('/')[-1])
+    return os.path.exists(f_name)
+
+def parse_cfg_argument(cfg_arg, params):
+    if not cfg_arg:
+        ci = params['cfg_i']
+        cf = params['cfg_f']
+        dc = params['cfg_d']
+    elif len(cfg_arg) == 1:
+        ci = int(cfg_arg[0])
+        cf = int(cfg_arg[0])
+        dc = 1
+    elif len(cfg_arg) == 3:
+        ci = int(cfg_arg[0])
+        cf = int(cfg_arg[1])
+        dc = int(cfg_arg[2])
+    else:
+        print('unrecognized use of cfg arg')
+        print('cfg_i [cfg_f cfg_d]')
+        sys.exit()
+    return range(ci,cf+dc,dc)
+
+def parse_cfg_src_argument(cfg_arg,src_arg,params,src_type=[]):
+    cfgs_run = parse_cfg_argument(cfg_arg,params)
+    if src_arg:
+        if len(cfgs_run) > 1:
+            print('if a src is passed, only 1 cfg can be specified: len(cfgs) = ',len(cfgs_run))
+            sys.exit(-1)
+        else:
+            srcs = {cfgs_run[0]:[src_arg]}
+            ''' make sure this src is in the expected list '''
+            no = str(cfgs_run[0])
+            src_check = []
+            srcs_cfg = sources.make(no, nl=params['NL'], nt=params['NT'], t_shifts=params['t_shifts'],
+                generator=params['generator'], seed=params['seed'][params['STREAM']])
+            for origin in srcs_cfg:
+                try:
+                    src_gen = srcs_cfg[origin].iteritems()
+                except AttributeError: # Python 3 automatically creates a generator
+                    src_gen = srcs_cfg[origin].items()
+                for src_type, src in src_gen:
+                    src_check.append(sources.xXyYzZtT(src))
+            if 'si' in params and 'sf' in params and 'ds' in params:
+                src_check = src_check[params['si']:params['sf']+params['ds']:params['ds']]
+            if srcs[cfgs_run[0]][0] not in src_check:
+                print('you supplied a src not in the current src list: allowed srcs')
+                print(src_check)
+                sys.exit(-1)
+    else:
+        srcs = {}
+        for c in cfgs_run:
+            no = str(c)
+            srcs_cfg = sources.make(no, nl=params['NL'], nt=params['NT'], t_shifts=params['t_shifts'],
+                generator=params['generator'], seed=params['seed'][params['STREAM']])
+            srcs[c] = []
+            for origin in srcs_cfg:
+                try:
+                    src_gen = srcs_cfg[origin].iteritems()
+                except AttributeError: # Python 3 automatically creates a generator
+                    src_gen = srcs_cfg[origin].items()
+                for s_type, src in src_gen:
+                    if len(src_type) > 0:
+                        if s_type in src_type:
+                            srcs[c].append(sources.xXyYzZtT(src))
+                    else:
+                        srcs[c].append(sources.xXyYzZtT(src))
+            if 'si' in params and 'sf' in params and 'ds' in params:
+                srcs[c] = srcs[c][params['si']:params['sf']+params['ds']:params['ds']]
+    return cfgs_run, srcs
+
+def nsq_vectors(nsq):
+    n = int(np.ceil(np.sqrt(nsq)))+1
+    r = range(-(n+1),n,1)
+    return  [ [x,y,z] for x in r for y in r for z in r if x**2+y**2+z**2 == nsq ]
+
+def p_lst(nsq):
+    n = int(np.ceil(np.sqrt(nsq)))+1
+    r = range(-(n+1),n,1)
+    n_lst = [ [x,y,z] for x in r for y in r for z in r if x**2+y**2+z**2 <= nsq ]
+    p_lst = []
+    for xyz in n_lst:
+        p_lst.append('px%d_py%d_pz%d' %(xyz[0],xyz[1],xyz[2]))
+    return p_lst
+
+def p_simple_lst(n=4):
+    r = [i for j in (range(-n,0), range(1,n+1)) for i in j]
+    p_lst = []
+    p_lst.append('px0_py0_pz0')
+    for p in r:
+        p_lst.append('px%d_py0_pz0' %p)
+        p_lst.append('px0_py%d_pz0' %p)
+        p_lst.append('px0_py0_pz%d' %p)
+    return p_lst
+
+def time_reverse(corr,phase=1,time_axis=1):
+    '''
+    Performe time-reversal of correlation function accounting for BC in time
+    phase     = +1 or -1
+    time_axis = time_axis in array
+    '''
+    if len(corr.shape) > 1:
+        if time_axis == 1:
+            cr = phase * np.roll(corr[:,::-1],1,axis=time_axis)
+            cr[:,0] = phase * cr[:,0]
+        else:
+            print('time_axis != 1 not supported at the moment')
+            sys.exit()
+    else:
+        cr = phase * np.roll(corr[::-1],1)
+        cr[0] = phase * cr[0]
+    return cr
+
+def get_write_h5_ff_data(f_in,f_out,p_in,p_out,d_name,overwrite=False,verbose=False):
+    data = f_in.get_node(p_in).read()
+    if not np.any(np.isnan(data)):
+        if d_name not in f_out.get_node(p_out):
+            f_out.create_array(p_out,d_name,data)
+            if verbose: print('    fresh collect')
+        elif d_name in f_out.get_node(p_out) and overwrite:
+            f_out.get_node(p_out+'/'+d_name)[:] = data
+            if verbose: print('    replace collect')
+        elif d_name in f_out.get_node(p_out) and not overwrite:
+            if verbose: print('    skipping, overwrite = False')
+
+def mom_avg(h5_data,state,mom_lst,weights=False):
+    '''
+    perform a momentum average of a state from an open h5 file
+    data file is assumed to be of shape [Nt,Nz,Ny,Nx,2] where 2 = [re,im]
+    data_mom = h5_data[state][:,pz,py,px]
+    '''
+    d_lst = []
+    w = []
+    for mom in mom_lst:
+        px,py,pz = mom['momentum']
+        w.append(mom['weight'])
+        #print(state)
+        d_lst.append(h5_data[state][:,pz,py,px])
+    d_lst = np.array(d_lst)
+    w = np.array(w)
+    if weights:
+        for wi,we in enumerate(w):
+            d_lst[wi] = we*d_lst[wi]
+        d_avg = np.sum(d_lst,axis=0) / np.sum(w)
+    else:
+        d_avg = np.mean(d_lst,axis=0)
+    return d_avg
