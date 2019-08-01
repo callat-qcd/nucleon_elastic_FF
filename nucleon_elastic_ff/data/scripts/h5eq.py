7030e54cc7b68e0ab05d46d8f856aec13399dda2
Christopher Koerber <christopher@ckoerber.com>
2019-07-31 17:30:51 -0700

    Added compare dset flag to h5eq

--- nucleon_elastic_ff/data/scripts/h5eq.py
+++ nucleon_elastic_ff/data/scripts/h5eq.py
@@ -3,10 +3,13 @@
 from argparse import ArgumentParser
 
 from nucleon_elastic_ff.data.h5io import assert_h5files_equal
+from nucleon_elastic_ff.data.h5io import assert_h5dsets_equal
 
 PARSER = ArgumentParser(
     description="Compare two hdf5 files."
-    " Checks if for each entry `|a - b| < atol + rtol * |b|`"
+    " Checks if for each entry `|acutal - expected| < atol + rtol * |expected|`"
+    " If one or both of `dset-actual`|`dset-expected` is given, just compares dsets."
+    " (Uses the same dset for both if just one is given.)"
 )
 PARSER.add_argument("actual", type=str, help="Address to actual file.")
 PARSER.add_argument("expected", type=str, help="Address to expected file.")
@@ -36,6 +39,18 @@ PARSER.add_argument(
     default=None,
     help="Group of expected file to use for comparison (if None compare all).",
 )
+PARSER.add_argument(
+    "--dset-actual",
+    type=str,
+    default=None,
+    help="Dset of actual file to use for comparison.",
+)
+PARSER.add_argument(
+    "--dset-expected",
+    type=str,
+    default=None,
+    help="Dset of expected file to use for comparison.",
+)
 
 
 def main():
@@ -43,14 +58,25 @@ def main():
     """
     args = PARSER.parse_args()
 
-    assert_h5files_equal(
-        args.actual,
-        args.expected,
-        atol=args.atol,
-        rtol=args.rtol,
-        group_actual=args.group_actual,
-        group_expected=args.group_expected,
-    )
+    if args.dset_actual or args.dset_expected:
+        assert_h5dsets_equal(
+            args.actual,
+            args.expected,
+            dset_actual=args.dset_actual or args.dset_expected,
+            dset_expected=args.dset_expected or args.dset_actual,
+            atol=args.atol,
+            rtol=args.rtol,
+        )
+
+    else:
+        assert_h5files_equal(
+            args.actual,
+            args.expected,
+            atol=args.atol,
+            rtol=args.rtol,
+            group_actual=args.group_actual,
+            group_expected=args.group_expected,
+        )
     print("[+] Files are equal!")
 
 
