a92748116aa8ea378b381d549059db3061199a6d
Andre Walker-Loud <awalker-loud@lbl.gov>
2020-05-18 18:16:33 -0400

    updating repair tape names

--- scripts/repair_Nsnk_Srcs_tape.py
+++ scripts/repair_Nsnk_Srcs_tape.py
@@ -6,15 +6,16 @@ ens_s = sys.argv[1]
 cfgs = {
     'a15m135XL':range(500,2996,5),
     'a09m135'  :range(78,3103,6),
-    'a12m130'  :'300-5295',
-    'a12m180L' :'800-1795',
-    'a09m220'  :'300-6300',
+    'a12m130'  :range(300,5296,5),
+    'a12m180L' :range(800,1796,5),
+    'a09m220'  :range(300,6301,6),
 }
 
 cfg_set = cfgs[ens_s.split('_')[0]]
 
 for cfg in cfg_set:
     no = str(cfg)
+    print(no)
     tape_file_querry = os.popen('hsi -P ls /proj/lgt100/c51/x_files/project_2/production/'+ens_s+'/formfac_4D_tslice_src_avg/'+no+' | grep formfac_4D | grep h5').read().split('\n')
     tape_file = []
     for t in tape_file_querry:
