d8e7df3282cb949891266c121003c06d42abb1d0
walkloud <awalker-loud@lbl.gov>
2020-10-29 17:17:06 -0700

    adding HMC charm mass to runs - just to be sure...

--- scripts/area51_files/a06m310L.py
+++ scripts/area51_files/a06m310L.py
@@ -26,7 +26,7 @@ params['MS_S'] = '0.024'
 params['MS_C'] = '0.286'
 params['NAIK'] = '-0.0533009'
 
-params['MC_reweight']       = [0.28038, 0.27476, 0.26914, 0.26352, 0.2579]
+params['MC_reweight']       = [0.286, 0.28038, 0.27476, 0.26914, 0.26352, 0.2579]
 params['MC_MAX_ITER']       = 500
 params['CC_Nnoise']         = 128
 params['MC_PROP_ERROR']     = '5.e-16'
